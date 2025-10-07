941cc6475d2c3005f2740197c1835e060d64b66b
austral <austral.ksb@gmail.com>
2025-10-07 10:34:45 +0300

    добавлен weather.py, выводящий скорость ветра и температуру

---
+++ weather.py
@@ -0,0 +1,9 @@
+# weather.py
+import random
+
+temperature = random.randint(-10, 30)
+wind_speed = random.randint(0, 20)
+
+print(f"Температура: {temperature} °C")
+print(f"Скорость ветра: {wind_speed} м/с")
+
